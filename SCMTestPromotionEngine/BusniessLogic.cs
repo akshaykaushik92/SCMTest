3ebb55405d2e1ad7cc9439e1f6d0a9762a19a350
Akshay Kaushik <akshaykaushikonline@gmail.com>
2020-09-19 21:00:53 +0530

    Added methods in DataLayer and BusinessLogic

--- SCMTestPromotionEngine/BusniessLogic.cs
+++ SCMTestPromotionEngine/BusniessLogic.cs
@@ -9,6 +9,30 @@ namespace SCMTestPromotionEngine
     {
         public double CalculateTotal(CheckoutQuantities checkoutQuantities)
         {
+            var priceSkuA = CalculateSkuAPrice(checkoutQuantities);
+            var priceSkuB = CalculateSkuBPrice(checkoutQuantities);
+            var priceSkuC = CalculateSkuCPrice(checkoutQuantities);
+            var priceSkuD = CalculateSkuDPrice(checkoutQuantities);
+            return priceSkuA + priceSkuB + priceSkuC + priceSkuD;
+        }
+
+        private double CalculateSkuAPrice(CheckoutQuantities cartQuantity)
+        {
+            return 0;
+        }
+
+        private double CalculateSkuBPrice(CheckoutQuantities cartQuantity)
+        {
+            return 0;
+        }
+
+        private double CalculateSkuCPrice(CheckoutQuantities cartQuantity)
+        {
+            return 0;
+        }
+
+        private double CalculateSkuDPrice(CheckoutQuantities cartQuantity)
+        {
             return 0;
         }
     }
--- SCMTestPromotionEngine/DataAccessLayer.cs
+++ SCMTestPromotionEngine/DataAccessLayer.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using SCMTestPromotionEngine.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SCMTestPromotionEngine
 {
     public class DataAccessLayer : MockDB
     {
+        public List<SkuPricing> GetPricing()
+        {
+            return this.skuPricingList;
+        }
 
+        public List<PromotionType> GetPromotionId(string skuType)
+        {
+            return this.promotionTypes.Where(x => x.ApplicableOnSku == skuType).ToList();
+        }
+
+        public List<PromotionType1> GetPromotionType1()
+        {
+            return this.promotionType1;
+        }
+
+        public List<PromotionType2> GetPromotionType2()
+        {
+            return this.promotionType2;
+        }
     }
 }
