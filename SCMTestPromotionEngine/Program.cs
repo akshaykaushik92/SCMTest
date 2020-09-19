785556c75f3842d4ff257b5519c8836e30481a11
Akshay Kaushik <akshaykaushikonline@gmail.com>
2020-09-19 19:57:09 +0530

    Blank Console application created

---
+++ SCMTestPromotionEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SCMTestPromotionEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
